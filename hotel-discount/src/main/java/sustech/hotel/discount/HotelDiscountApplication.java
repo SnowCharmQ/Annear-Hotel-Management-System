2ec4911188bc766914db9acf7b02ba078464c79a
SnowCharm <619022098@qq.com>
2022-09-27 22:06:25 +0800

    add module hotel-discount

---
+++ hotel-discount/src/main/java/sustech/hotel/discount/HotelDiscountApplication.java
@@ -0,0 +1,13 @@
+package sustech.hotel.discount;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotelDiscountApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HotelDiscountApplication.class, args);
+    }
+
+}
---
+++ hotel-discount/src/test/java/sustech/hotel/discount/HotelDiscountApplicationTests.java
@@ -0,0 +1,13 @@
+package sustech.hotel.discount;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotelDiscountApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

0048ff45f6bc6caab3df2cc15524f6958b8b0920
SnowCharm <619022098@qq.com>
2022-09-27 22:02:39 +0800

    add module hotel-member

---
+++ hotel-member/src/main/java/sustech/hotel/member/HotelMemberApplication.java
@@ -0,0 +1,13 @@
+package sustech.hotel.member;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotelMemberApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HotelMemberApplication.class, args);
+    }
+
+}

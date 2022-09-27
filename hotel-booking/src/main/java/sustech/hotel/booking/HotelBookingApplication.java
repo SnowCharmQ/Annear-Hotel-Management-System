79ddfa6c388fca4181f378bf889ae7f7a8632764
SnowCharm <619022098@qq.com>
2022-09-27 23:07:12 +0800

    add module hotel-booking

---
+++ hotel-booking/src/main/java/sustech/hotel/booking/HotelBookingApplication.java
@@ -0,0 +1,13 @@
+package sustech.hotel.booking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotelBookingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HotelBookingApplication.class, args);
+    }
+
+}
---
+++ hotel-booking/src/test/java/sustech/hotel/booking/HotelBookingApplicationTests.java
@@ -0,0 +1,13 @@
+package sustech.hotel.booking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotelBookingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

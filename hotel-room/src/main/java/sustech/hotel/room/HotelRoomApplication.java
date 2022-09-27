df93953f6cba809d1517549f41fb856a7a505b12
SnowCharm <619022098@qq.com>
2022-09-27 22:12:59 +0800

    add module hotel-room

---
+++ hotel-room/src/main/java/sustech/hotel/room/HotelRoomApplication.java
@@ -0,0 +1,13 @@
+package sustech.hotel.room;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotelRoomApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HotelRoomApplication.class, args);
+    }
+
+}
---
+++ hotel-room/src/test/java/sustech/hotel/room/HotelRoomApplicationTests.java
@@ -0,0 +1,13 @@
+package sustech.hotel.room;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotelRoomApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

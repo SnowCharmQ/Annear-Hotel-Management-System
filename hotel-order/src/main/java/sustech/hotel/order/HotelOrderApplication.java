b92afd2e492ba774c55193ee382ab40f8e55914c
SnowCharm <619022098@qq.com>
2022-09-27 22:08:43 +0800

    add module hotel-order

---
+++ hotel-order/src/main/java/sustech/hotel/order/HotelOrderApplication.java
@@ -0,0 +1,13 @@
+package sustech.hotel.order;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotelOrderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HotelOrderApplication.class, args);
+    }
+
+}
---
+++ hotel-order/src/test/java/sustech/hotel/order/HotelOrderApplicationTests.java
@@ -0,0 +1,13 @@
+package sustech.hotel.order;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotelOrderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

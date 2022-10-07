c9ed6e91fc0cde28d223c51881c8d1c9a277019b
SnowCharm <619022098@qq.com>
2022-10-07 09:49:44 +0800

    add module hotel-gateway

---
+++ hotel-gateway/src/main/java/sustech/hotel/gateway/HotelGatewayApplication.java
@@ -0,0 +1,13 @@
+package sustech.hotel.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotelGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HotelGatewayApplication.class, args);
+    }
+
+}
---
+++ hotel-gateway/src/test/java/sustech/hotel/gateway/HotelGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package sustech.hotel.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotelGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

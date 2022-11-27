39aca2804e5db7d921fc0c71bb214f65735e5ed8
SnowCharm <619022098@qq.com>
2022-11-28 01:12:01 +0800

    update the port for admin to 8096

--- hotel-fronted-admin/vue.config.js
+++ hotel-fronted-admin/vue.config.js
@@ -1,5 +1,10 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-  lintOnSave:false
+  lintOnSave:false,
+  devServer: {
+    port: 8096,
+    open: true,
+    hot: true
+  }
 })

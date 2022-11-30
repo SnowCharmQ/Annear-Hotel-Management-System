cbf245eb0dcf9c64a41d43dacdbbbddfd0611b9d
sui_h <suih2002@outlook.com>
2022-12-01 03:12:24 +0800

    update alipay timeout

--- hotel-order/src/main/java/sustech/hotel/order/config/AlipayTemplate.java
+++ hotel-order/src/main/java/sustech/hotel/order/config/AlipayTemplate.java
@@ -37,7 +37,7 @@ public class AlipayTemplate {
     private String charset = "utf-8";
 
     //订单超时时间
-    private String timeout = "1m";
+    private String timeout = "10m";
 
     // 支付宝网关； https://openapi.alipaydev.com/gateway.do
     public String gatewayUrl = "https://openapi.alipaydev.com/gateway.do";

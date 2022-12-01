ed95557f418fc04b51aa01b35665f8a282c8bdb1
sui_h <suih2002@outlook.com>
2022-12-01 20:53:56 +0800

    add automatic update order

--- hotel-order/src/main/java/sustech/hotel/order/service/impl/OrderServiceImpl.java
+++ hotel-order/src/main/java/sustech/hotel/order/service/impl/OrderServiceImpl.java
@@ -7,11 +7,11 @@ import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.data.redis.core.script.DefaultRedisScript;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
 import java.math.BigDecimal;
 import java.sql.Date;
-import java.sql.Time;
 import java.sql.Timestamp;
 import java.util.*;
 import java.util.concurrent.TimeUnit;
@@ -26,7 +26,6 @@ import sustech.hotel.exception.ExceptionCodeEnum;
 import sustech.hotel.exception.auth.UserNotFoundException;
 import sustech.hotel.exception.order.*;
 import sustech.hotel.exception.others.InvalidDateException;
-import sustech.hotel.model.to.hotel.HotelTo;
 import sustech.hotel.model.to.hotel.RoomInfoTo;
 import sustech.hotel.model.to.hotel.RoomTo;
 import sustech.hotel.model.to.hotel.RoomTypeTo;
@@ -57,6 +56,9 @@ public class OrderServiceImpl extends ServiceImpl<OrderDao, OrderEntity> impleme
 
     @Autowired
     private MemberFeignService memberFeignService;
+
+    @Autowired
+    private OrderDao orderDao;
 
     @Autowired
     private RoomFeignService roomFeignService;
@@ -266,7 +268,8 @@ public class OrderServiceImpl extends ServiceImpl<OrderDao, OrderEntity> impleme
         }
     }
 
-
-
-
+    @Scheduled(cron = "0 1 12 * * ?")
+    public void automaticUpdateOrderStatus() {
+        orderDao.automaticUpdateOrderStatus();
+    }
 }
